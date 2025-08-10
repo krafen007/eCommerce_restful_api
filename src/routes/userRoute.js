00f968e43253ec3c0c4bb6a71a731679881c9e8d
krafen007 <noahshafaei@gmail.com>
2025-08-10 19:14:06 +0300

    Add services section and update user model, controller, route

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,18 +1,9 @@
 import express from "express";
-import { logIn, register } from "./../controllers/userController.js";
-import { validateJWT } from "../middlewares/validateJWT.js";
+import { logIn, register } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", logIn);
-router.get("/profile", validateJWT, (req, res) => {
-  const user = req.user;
-
-  res.status(200).json({
-    message: "Welcome to your profile",
-    data: user,
-  });
-});
 
 export default router;
